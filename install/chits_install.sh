70b0674b80b0b5a5ce5c9bc86be3a4c112e4eb26
Alison Perez <perez.alison@gmail.com>
2009-10-07 22:49:34 +0800

    modified:   install/chits_install.sh 	modified:   install/php.ini.sample

--- install/chits_install.sh
+++ install/chits_install.sh
@@ -11,6 +11,7 @@ wget -O /etc/php5/apache2/php.ini http://github.com/mikeymckay/chits/raw/master/
 #no sudo
 su $SUDO_USER -c "git clone git://github.com/mikeymckay/chits.git /var/www/chits"
 su $SUDO_USER -c "cp /var/www/chits/modules/_dbselect.php.sample /var/www/chits/modules/_dbselect.php"
+echo "You will now be prompted for your mysql root user password a few times. This is the one you entered in the blue box when you installed mysql."
 echo "CREATE DATABASE example_database;" | mysql -u root -p
 mysql -u root -p example_database < /var/www/chits/db/core_data.sql
 echo "INSERT INTO user SET user='example_user',password=password('example_password'),host='localhost';FLUSH PRIVILEGES;GRANT ALL PRIVILEGES ON example_database.* to example_user@localhost IDENTIFIED BY 'example_password';" | mysql -u root mysql -p
@@ -25,6 +26,7 @@ echo "Installing testing tools"
 gem install cucumber mechanize rspec webrat --no-ri
 
 echo "Setting up test database"
+echo "You will again be prompted for your mysql root user password a few times. This is the one you entered in the blue box when you installed mysql."
 echo "CREATE DATABASE chits_testing;" | mysql -u root -p;
 echo "GRANT ALL PRIVILEGES ON chits_testing.* TO chits_tester@localhost IDENTIFIED BY 'useless_password'" | mysql -u root -p
 mysql -u chits_tester --password=useless_password chits_testing < /var/www/chits/features/support/../../db/core_data.sql
