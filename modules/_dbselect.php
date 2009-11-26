7722257597de665c3cd1d75a1b29f2c4ddc19af2
Alison Perez <perez.alison@gmail.com>
2009-11-26 22:13:52 +0800

    new file:   _dbselect.php

---
+++ modules/_dbselect.php
@@ -0,0 +1,9 @@
+<?php
+// Rename this file to _dbselect.php and update the values below to match your own database username/password
+session_start();
+$_SESSION["dbname"] = "chits"; // Name of the database (probably chits)
+$_SESSION["dbuser"] = "root"; // mysql username that you are using to connect to the database
+$_SESSION["dbpass"] = "root"; // mysql password that you are using to connect to the database
+$conn = mysql_connect("localhost", $_SESSION["dbuser"], $_SESSION["dbpass"]);
+$db->connectdb($_SESSION["dbname"]);
+?>
