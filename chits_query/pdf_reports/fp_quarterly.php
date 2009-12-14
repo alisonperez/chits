470006a12247f6dec56ea934de91d2ef5d51ddb5
Alison Perez <perez.alison@gmail.com>
2009-12-14 14:51:02 +0800

    new file:   chits_query/pdf_reports/fp_quarterly.php

---
+++ chits_query/pdf_reports/fp_quarterly.php
@@ -0,0 +1,157 @@
+<?php
+
+session_start();
+
+ob_start();
+
+require('./fpdf/fpdf.php');
+
+
+$db_conn = mysql_connect("localhost","$_SESSION[dbuser]","$_SESSION[dbpass]");
+mysql_select_db($_SESSION[dbname]);
+
+class PDF extends FPDF
+{
+	var $widths;
+	var $aligns;
+	var $page;	
+	
+	
+function SetWidths($w)
+{
+    //Set the array of column widths
+    $this->widths=$w;
+}
+
+function SetAligns($a)
+{
+    //Set the array of column alignments
+    $this->aligns=$a;
+}
+
+function Row($data)
+{
+    //Calculate the height of the row
+    $nb=0;
+    for($i=0;$i<count($data);$i++)
+        $nb=max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+    $h=5*$nb;
+    //Issue a page break first if needed
+    $this->CheckPageBreak($h);
+    //Draw the cells of the row
+    for($i=0;$i<count($data);$i++)
+    {
+        $w=$this->widths[$i];
+        $a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'C'; //sets the alignment of text inside the cell
+        //Save the current position
+        $x=$this->GetX();
+        $y=$this->GetY();
+        //Draw the border
+        $this->Rect($x,$y,$w,$h);
+        //Print the text
+        $this->MultiCell($w,5,$data[$i],0,$a);
+        //Put the position to the right of the cell
+        $this->SetXY($x+$w,$y);
+    }
+    //Go to the next line
+    $this->Ln($h);
+}
+
+
+function CheckPageBreak($h)
+{
+    //If the height h would cause an overflow, add a new page immediately
+    if($this->GetY()+$h>$this->PageBreakTrigger)
+        $this->AddPage($this->CurOrientation);
+}
+
+
+function NbLines($w,$txt)
+{
+    //Computes the number of lines a MultiCell of width w will take
+    $cw=&$this->CurrentFont['cw'];
+    if($w==0)
+        $w=$this->w-$this->rMargin-$this->x;
+    $wmax=($w-2*$this->cMargin)*1000/$this->FontSize;
+    $s=str_replace("\r",'',$txt);
+    $nb=strlen($s);
+    if($nb>0 and $s[$nb-1]=="\n")
+        $nb--;
+    $sep=-1;
+    $i=0;
+    $j=0;
+    $l=0;
+    $nl=1;
+    while($i<$nb)
+    {
+        $c=$s[$i];
+        if($c=="\n")
+        {
+            $i++;
+            $sep=-1;
+            $j=$i;
+            $l=0;
+            $nl++;
+            continue;
+        }
+        if($c==' ')
+            $sep=$i;
+        $l+=$cw[$c];
+        if($l>$wmax)
+        {
+            if($sep==-1)
+            {
+                if($i==$j)
+                    $i++;
+            }
+            else
+                $i=$sep+1;
+            $sep=-1;
+            $j=$i;
+            $l=0;
+            $nl++;
+        }
+        else
+            $i++;
+    }
+    return $nl;
+}
+
+
+function Header()
+{
+    $this->SetFont('Arial','B','12');
+    $this->Cell(0,5,'FHSIS REPORT FOR THE QUARTER:'.$_SESSION[quarter]."\t\tYEAR: ".$_SESSION[year],0,1,C);
+    $this->Cell(0,5, 'MUNICAPLITY/CITY NAME: '.$_SESSION[datanode][name]);
+    $this->Cell(0,5,)
+
+
+}
+
+
+function Footer(){
+    $this->SetY(-15);
+    //Arial italic 8
+    $this->SetFont('Arial','I',8);
+    //Page number
+    $this->Cell(0,10,$this->PageNo().'/{nb}',0,0,'C');
+}
+
+
+
+}
+
+$pdf = new PDF('L','mm','Legal');
+$util = new util();
+
+$pdf->AliasNbPages();
+$pdf->SetFont('Arial','',10);
+
+$pdf->AddPage();
+
+
+//$pdf->AddPage();
+//$pdf->show_fp_summary();
+$pdf->Output();
+
+?>
