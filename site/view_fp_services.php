490cf80e64517b69bc4fa2b2c908fdff1b69a477
alison perez <perez.alison@gmail.com>
2010-01-17 19:26:15 +0800

    added CSS for the table of services

--- site/view_fp_services.php
+++ site/view_fp_services.php
@@ -12,11 +12,11 @@
   if(mysql_num_rows($q_fp_service)!=0):
     
     echo "Method: ".$_GET[method_id];
-    echo "<table border='1'>";
-    echo "<tr align='center'><td>Date of Service</td><td>Source</td><td>Remarks</td><td>Next Service Date</td></tr>";
+    echo "<table bgcolor='#66FF66'>";
+    echo "<tr align='center' bgcolor='#339966' style='font-family: verdana, arial, sans serif; font-size: 8pt; font-weight: bold'><td>Date of Service</td><td>Source</td><td>Remarks</td><td>Next Service Date</td></tr>";
     
     while(list($date_service,$source_name,$remarks,$next_service_date)=mysql_fetch_array($q_fp_service)){
-      echo "<tr>";
+      echo "<tr style='font-family: verdana, arial, sans serif; font-size: 8pt; font-weight: bold'>";
       echo "<td>".$date_service."</td>";
       echo "<td>".$source_name."</td>";
       echo "<td>".$remarks."</td>";
