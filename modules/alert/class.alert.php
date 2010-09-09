954d68dd5d075816e64bc29491cd6a6ea1d31cca
alison perez <perez.alison@gmail.com>
2010-09-09 12:53:11 +0800

    ALERT: created a bare ALERT administration table with basic fields needed. No logic yet

--- modules/alert/class.alert.php
+++ modules/alert/class.alert.php
@@ -7,6 +7,8 @@ class alert extends module{
 		$this->version = "0.1-".date('Y-m-d');
 		$this->authod = "darth_ali";
 		$this->module = "alert";
+		
+		$this->mods = array('mc'=>array("Maternal Care"),'ccdev'=>array("Expanded Program for Immunization"),'fp'=>array("Birth Spacing / Family Planning"));
 	}
 
 
@@ -67,6 +69,113 @@ class alert extends module{
 	
 	function _alert_type(){
 		echo "this is the container for the alert and reminder adminstration interface.";
+		echo "<table border='1'>";
+		echo "<tr><td width='65%'>";
+		
+		echo "<table>";
+		echo "<thead colspan='2'>REMINDER & ALERT ADMINISTRATION</thead>";
+		
+		echo "<tr>";		
+		echo "<td>Reminder/Alert Label</td>";
+		echo "<td>";
+		echo "<input type='text' name='txt_label' size='25'></input>";
+		echo "</td>";								
+		echo "</tr>";
+		
+		echo "<tr>";
+		echo "<td>Health Program</td>";		
+		echo "<td>";
+		echo "<select name='sel_mods' size='1'>";
+		foreach($this->mods as $key=>$value){
+			foreach($value as $key2=>$value2){
+				echo "<option value='$key'>$value2</option>";
+				
+			}
+		}
+		echo "</select>";
+		echo "</td>";
+		echo "</tr>";
+
+		echo "<tr>";
+		echo "<td valign='top'>Reminder/Alert Message</td>";
+		echo "<td>";
+		echo "<textarea name='txt_msg' cols='25' rows='3'>";
+		echo "</textarea>";
+		echo "</td>";
+		echo "</tr>";
+
+		echo "<tr>";
+		echo "<td valign='top'>Recommended Actions</td>";
+		echo "<td>";
+		echo "<textarea name='txt_msg' cols='25' rows='3'>";
+		echo "</textarea>";
+		echo "</td>";
+		echo "</tr>";
+
+		echo "<tr>";
+		echo "<td valign='top'>No. of Days Reminder is posted before base date</td>";
+		echo "<td>";
+		echo "<select name='sel_days_before' size='1'>";
+		
+		for($i=0;$i<=100;$i++){
+			echo "<option value='$i'>$i</option>";
+		}
+		
+		echo "</select>";		
+		echo "&nbsp;&nbsp;days</td>";
+		echo "</tr>";
+
+		echo "<tr>";
+		echo "<td>No. of Days Reminder is posted after base date</td>";
+		echo "<td>";
+		echo "<select name='sel_days_after' size='1'>";
+		
+		for($i=0;$i<=100;$i++){
+			echo "<option value='$i'>$i</option>";
+		}
+		echo "</select>";		
+		echo "&nbsp;&nbsp;days</td>";
+		echo "</tr>";
+
+		echo "<tr>";
+		echo "<td></td>";
+		echo "</tr>";
+
+		echo "<tr>";
+		echo "<td>Base Date for Reminder/Alert</td>";
+		echo "<td>";
+		echo "<select name='sel_base_date' size='1'>";  //list will display date fields based on selected health program
+		
+		echo "<option value='test'>test date</option>";
+		echo "</select>";
+		echo "</td>";
+		echo "</tr>";
+
+		echo "<tr>";
+		echo "<td>URL for data entry</td>";
+		echo "<td>";
+		echo "<input type='text' name='txt_label' size='25'></input>";
+		echo "</td>";
+		echo "</tr>";
+
+		echo "</table>";
+
+		echo "</td>";
+
+		echo "<td>";
+		echo '&nbsp;&nbsp;&nbsp;&nbsp;';
+		echo "</td>";
+
+
+		echo "<td>";
+		
+		echo "<table>";
+		echo "<thead>LIST of REMINDERS & ALERTS</thead>";
+		echo "</table>";
+		
+		echo "</td>";
+
+		echo "</table>";
 	}
 
 	function _alert(){
