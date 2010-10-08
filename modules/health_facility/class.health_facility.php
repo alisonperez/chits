d80687ba0ca700c4776edd1c4e00a76b09fc6ca6
alison perez <perez.alison@gmail.com>
2010-10-08 12:49:50 +0800

    HEALTH FACILITY: finished doing the interface for health facility-brgy mapping

--- modules/health_facility/class.health_facility.php
+++ modules/health_facility/class.health_facility.php
@@ -44,7 +44,59 @@ class health_facility extends module{
 	//----- CUSTOM BUILT FUNCTION ---- //
 	
 	function _health_facility(){
-		echo "nosila";
+		$q_health_facility = mysql_query("SELECT facility_id, facility_name FROM m_lib_health_facility ORDER by facility_name ASC") or die("Cannot query 47 ".mysql_error());
+
+		$q_barangay = mysql_query("SELECT barangay_id, barangay_name FROM m_lib_barangay ORDER BY barangay_name ASC") or die("Cannot query 47 ".mysql_error());
+
+		echo "<form action='$_SERVER[PHP_SELF]?page=$_GET[page]&menu_id=$_GET[menu_id]' method='POST' name='form_health_facility'>";
+
+		echo "<table border='1'>";
+		echo "<tr><td colspan='2'>HEALTH FACILITY - BARANGAY MAPPING OF CATCHMENT AREA</td></tr>";
+		echo "<tr>";
+		
+		echo "<td valign='top' colspan='2'>Health Facility&nbsp;&nbsp;<select name='sel_health_facility' size='1'>";
+		
+		while(list($fac_id,$fac_name)=mysql_fetch_array($q_health_facility)){
+			echo "<option value='$fac_id'>$fac_name</option>";
+		}
+		echo "</select></td>";
+		
+		/*echo "<td valign='top'>Barangay&nbsp;&nbsp;<select name='sel_baragay' size='5' MULTIPLE>";
+		while(list($barangay_id,$barangay_name)=mysql_fetch_array($q_barangay)){
+			echo "<option value='$barangay_id'>$barangay_name</option>";
+		}
+		echo "</select></td>";*/
+		//echo "<td><input type=>"
+		echo "</tr>";
+
+		echo "<tr>";
+		echo "<td valign='center'>Select Barangay </td>";
+		echo "<td>";
+		echo "<table>";
+		
+		while(list($barangay_id,$barangay_name)=mysql_fetch_array($q_barangay)){
+			$i += 1;
+			echo "<tr>";			
+			echo "<select name='sel_baragay' size='10' MULTIPLE>";
+			while(list($barangay_id,$barangay_name)=mysql_fetch_array($q_barangay)){
+				echo "<option value='$barangay_id'>$barangay_name</option>";
+			}
+			echo "</select></td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+		echo "</td>";
+		echo "</tr>";
+
+		echo "<tr>";
+		echo "<td colspan='2' align='center'><input type='submit' name='submit_brgy' value='Assign Barangay/s to Health Facility' />&nbsp;&nbsp;&nbsp;<input type='reset' name='reset_brgy' value='Reset'></td>";
+		echo "</tr>";
+
+		echo "</table>";
+
+		echo "</form>";
+
+		
 	}
 
 
