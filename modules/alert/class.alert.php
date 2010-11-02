82768de105607bbc13c59ce0ac37354dcb6ef184
alison perez <perez.alison@gmail.com>
2010-11-02 13:26:44 +0800

    ALERT: added alert definition for IUD checking follow-up visit

--- modules/alert/class.alert.php
+++ modules/alert/class.alert.php
@@ -726,6 +726,25 @@ class alert extends module{
 					case '23':			//condom re-supply reminder
 						break;
 
+					case '24':			//IUD follow-up
+
+						$q_fp = mysql_query("SELECT fp_px_id,date_registered FROM m_patient_fp_method WHERE patient_id='$patient_id' AND method_id='IUD' AND drop_out='N' ORDER by date_registered DESC") or die("Cannot query 710 ".mysql_error());
+
+						if(mysql_num_rows($q_fp)!=0):
+							list($fp_px_id,$date_registered) = mysql_fetch_array($q_fp);
+							
+							$q_fp_method = mysql_query("SELECT fp_service_id,(to_days(next_service_date)-to_days('$date_today')) as sum_date FROM m_patient_fp_method_service WHERE fp_px_id='$fp_px_id' AND patient_id='$patient_id' AND (to_days(next_service_date)-to_days('$date_today')) BETWEEN 0 AND '$days_before'") or die("Cannot query 714 ".mysql_error());
+
+							//echo mysql_num_rows($q_fp_method);
+
+							if(mysql_num_rows($q_fp_method)!=0):
+								list($fp_service_id) = mysql_fetch_array($q_fp_method);
+								array_push($arr_case_id,$fp_service_id);
+							endif;
+						endif;
+
+						break;
+
 					default:			
 
 						break;
